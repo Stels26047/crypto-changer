e954cd10416882846958e678d6a4d205b2001f15
StelsMaster <58134253+Stels26047@users.noreply.github.com>
2025-01-09 22:07:15 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -54,9 +54,9 @@
         else if(this.cryptoFirst == 'ETH' && this.cryptoSecond == 'USDT')
           this.result = convert.BTC.USDT(this.amount);
         else if(this.cryptoFirst == 'USDT' && this.cryptoSecond == 'BTC')
-          this.result = convert.BTC.ETH(this.amount);
+          this.result = convert.USDT.BTC(this.amount);
         else if(this.cryptoFirst == 'USDT' && this.cryptoSecond == 'ETH')
-          this.result = convert.BTC.USDT(this.amount);
+          this.result = convert.USDT.ETH(this.amount);
       }
     }
   }
